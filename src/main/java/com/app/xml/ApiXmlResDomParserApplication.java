5808d90d36091dd18e201174deec29f4cfd77090
pratikw1859 <pratikw1859@gmail.com>
2019-11-26 19:55:19 +0530

    First Commit

---
+++ src/main/java/com/app/xml/ApiXmlResDomParserApplication.java
@@ -0,0 +1,13 @@
+package com.app.xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiXmlResDomParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiXmlResDomParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/xml/ApiXmlResDomParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiXmlResDomParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
